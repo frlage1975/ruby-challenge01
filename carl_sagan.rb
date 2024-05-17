939f79de5622072ec663bb206a05331e4dec024b
Felipe Rodriguez <frlage@LAPTOP-1766LIDU>
2024-05-17 15:14:52 -0500

    added a dot at the end of the string in question 1

--- carl_sagan.rb
+++ carl_sagan.rb
@@ -27,7 +27,7 @@ sagan = [
 
 puts "\n---------------------------------- Question 1 ----------------------------------\n\n"
 
-puts "#{carl[:words][2].capitalize} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}"
+puts "#{carl[:words][2].capitalize} #{sagan[0][:are]} #{sagan[0]['A']} #{sagan[1]['waaaaaay']} #{carl[:words][1]} #{sagan[1][:th3]} #{carl[:toast]} #{sagan[3][2][0]} #{carl[:words][0]} #{sagan[2]}#{carl[:punctuation][1]}"
 
 puts "\n---------------------------------- Question 2 ----------------------------------\n\n"
 
